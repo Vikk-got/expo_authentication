62ff6db8c25b8143a71be0f1db01720643b30d33
vatsal <167070083+Vikk-got@users.noreply.github.com>
2026-02-08 19:29:36 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,13 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAf5Drdvfm_t6fYhgMk9tAr5517XSp6NTk",
-    authDomain: "vatsal-auth-app.firebaseapp.com",
-    projectId: "vatsal-auth-app",
-    storageBucket: "vatsal-auth-app.firebasestorage.app",
-    messagingSenderId: "186592728989",
-    appId: "1:186592728989:web:09f7d2844af59370624174",
-    measurementId: "G-3DJMNL7NQ3"
+add your own 
 };
 
 // Initialize Firebase
